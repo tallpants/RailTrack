523ec2c800d275090b56036434df0e021fd74b0b
adithya vs <adithyasubramaniam27@gmail.com>
2018-03-08 20:10:59 +0530

    route list

--- src/api/config.ts
+++ src/api/config.ts
@@ -11,7 +11,7 @@ const baseURL = "http://indianrailapi.com/api/v1";
  * API key for indianrailapi.com
  * On free tier, must be regenerated every 24 hours at http://indianrailapi.com/IndianRail/ApiKey
  */
-export const key = "9f757be96528bff318b5093123ce5912"; // TODO: Externalize this.
+export const key = "d040a6d701ddc01dc36662b8a62f83e6"; // TODO: Externalize this.
 
 /**
  * Axios HTTP client with the base URL configured to railwayapi.com's URL.
---
+++ src/api/route.ts
@@ -0,0 +1,41 @@
+import { httpClient, key } from "./config"
+import { AxiosResponse } from "axios"
+import { error } from "util";
+
+interface IRouteResponse{
+
+    ResponseCode: number
+
+    TrainName: {
+
+        Name: string
+    }
+
+    RouteList: Array<{ FullName: string }>
+}
+
+class RouteStatus{
+    public trainName: string
+    public route: Array<string>
+
+    constructor(apiResponse: IRouteResponse){
+        this.trainName = apiResponse.TrainName.Name
+        this.route = apiResponse.RouteList.map( station => station.FullName)
+    }
+}
+
+type RouteErrorReason = "not found"
+
+export default async function getRouteList(trainNumber: string): Promise<{ data?:RouteStatus; error?:RouteErrorReason }> {
+    const response: AxiosResponse<IRouteResponse> = await httpClient.get(`/pnrstatus/apikey/${key}/trainno/${trainNumber}`);
+
+    switch(response.data.ResponseCode)
+    {
+        case 200:
+        return { data: new RouteStatus(response.data), error: null }
+
+        case 404:
+        return { data:null,error:"not found" }
+    }
+    
+}
