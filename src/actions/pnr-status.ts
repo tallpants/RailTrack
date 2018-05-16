470f7783f26184b744699abf0c18e1ca38f1c0f7
tallpants <adithyakreddy6@gmail.com>
2018-05-16 11:22:04 +0530

    Removed some comments

--- src/actions/pnr-status.ts
+++ src/actions/pnr-status.ts
@@ -2,8 +2,8 @@
  * Exports the intent handler / action for the PNR status intent.
  */
 
-import { DialogflowApp } from "actions-on-google";
-import getPNRStatus from "../api/pnr";
+import { DialogflowApp } from 'actions-on-google';
+import getPNRStatus from '../api/pnr';
 
 /**
  * Extract the PNR number from the intent, get the PNR status, and respond with a
@@ -12,7 +12,7 @@ import getPNRStatus from "../api/pnr";
  */
 export default async function pnrStatusAction(app: DialogflowApp) {
   // Extract the PNR number from the intent.
-  const pnrNumber: any = app.getArgument("pnr");
+  const pnrNumber: any = app.getArgument('pnr');
 
   // Try to get the PNR status from the API.
   const response = await getPNRStatus(pnrNumber);
@@ -20,20 +20,19 @@ export default async function pnrStatusAction(app: DialogflowApp) {
   // If there was an error, respond with a string describing the error.
   if (response.error) {
     switch (response.error) {
-      case "flushed":
-        app.tell("Sorry, that PNR number has already been flushed.");
+      case 'flushed':
+        app.tell('Sorry, that PNR number has already been flushed.');
         break;
-      case "invalid":
+      case 'invalid':
         app.tell("Sorry, that isn't a valid PNR number.");
         break;
-      case "notfound":
+      case 'notfound':
         app.tell(
-          "Sorry, I couldn't find any information about this PNR number. Are you sure it's correct?"
+          "Sorry, I couldn't find any information about this PNR number. Are you sure it's correct?",
         );
     }
   } else {
     // Build a response string with the data from the API response and send it to the user.
-    // TODO: Look into refactoring this spaghettij
     const date: string = `This journey is scheduled on <say-as interpret-as="date" format="dmy" detail="1">${
       response.data.journeyDate
     }</say-as>`;
@@ -52,7 +51,7 @@ export default async function pnrStatusAction(app: DialogflowApp) {
     let statuses: string;
 
     if (response.data.numPassengers === 1) {
-      numPassengers = "There is one passenger";
+      numPassengers = 'There is one passenger';
       statuses = `Status is <say-as interpret-as="characters">${
         response.data.statuses[0]
       }</say-as>`;
@@ -60,11 +59,11 @@ export default async function pnrStatusAction(app: DialogflowApp) {
       numPassengers = `There are ${response.data.numPassengers} passengers`;
       statuses = `Their statuses are ${response.data.statuses
         .map(status => `<say-as interpret-as="characters">${status}</say-as>`)
-        .join(", ")}`;
+        .join(', ')}`;
     }
 
     app.ask(
-      `<speak>${date}, ${route}, ${train}. ${numPassengers}. ${statuses}.</speak>`
+      `<speak>${date}, ${route}, ${train}. ${numPassengers}. ${statuses}.</speak>`,
     );
   }
 }
--- src/intent-map.ts
+++ src/intent-map.ts
@@ -1,22 +1,17 @@
-import backendStatusAction from "./actions/backend-status";
-import pnrStatusAction from "./actions/pnr-status";
-import liveStatusAction from "./actions/live-status";
-import routeAction from "./actions/route";
+import backendStatusAction from './actions/backend-status';
+import pnrStatusAction from './actions/pnr-status';
+import liveStatusAction from './actions/live-status';
+import routeAction from './actions/route';
 
 /**
  * Mapping from Dialogflow intents (specifically the action name associated
  * with the intent) to handlers defined in `actions.ts`.
- *
- * @example
- * intentMap.set('BACKEND_STATUS', actions.backendStatus);
- * // When the intent's action name is 'BACKEND_STATUS', handle it with
- * // the `backendStatus` function defined in `/actions`.
  */
 const intentMap = new Map();
 
-intentMap.set("BACKEND_STATUS", backendStatusAction);
-intentMap.set("PNR_STATUS", pnrStatusAction);
-intentMap.set("LIVE_STATUS", liveStatusAction);
-intentMap.set("ROUTE", routeAction);
+intentMap.set('BACKEND_STATUS', backendStatusAction);
+intentMap.set('PNR_STATUS', pnrStatusAction);
+intentMap.set('LIVE_STATUS', liveStatusAction);
+intentMap.set('ROUTE', routeAction);
 
 export default intentMap;
